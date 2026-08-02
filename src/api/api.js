0206d93c944287ddb8395afdd84cacd91824b29e
FahmiMoch <fahmifadillah18@gmail.com>
2026-08-02 22:07:12 +0700

    ganti alamat api

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL || "http://localhost:8000",
+    import.meta.env.VITE_API_URL ||
+    "https://backend-ritles-4o9y-pi.vercel.app",
 
   timeout: 10000,
 
