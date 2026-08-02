f7c03ee42d5506c1b241f45f17eeb00f289953f0
FahmiMoch <fahmifadillah18@gmail.com>
2026-08-03 03:40:21 +0700

    fix auth again

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,19 +2,24 @@
 // Login
 // ======================
 export async function loginUser(data) {
+
 try {
 
+
 const formData = new URLSearchParams();
+
 
 formData.append(
   "username",
   data.email
 );
 
+
 formData.append(
   "password",
   data.password
 );
+
 
 
 const response = await api.post(
@@ -29,39 +34,82 @@ const response = await api.post(
 );
 
 
+
 // Karena interceptor sudah return response.data
 const tokenData = response;
 
 
-if(tokenData?.access_token){
 
-  localStorage.setItem(
-    "access_token",
-    tokenData.access_token
-  );
+console.log(
+  "LOGIN RESPONSE:",
+  tokenData
+);
 
 
-  localStorage.setItem(
-    "user",
-    JSON.stringify(
-      tokenData.user || {}
-    )
-  );
+
+// ======================
+// Validasi Token
+// ======================
+if(!tokenData?.access_token){
+
+
+  return {
+
+
+    success:false,
+
+
+    message:
+    "Email atau password salah"
+
+
+  };
+
 
 }
 
 
+
+// ======================
+// Simpan Session
+// ======================
+localStorage.setItem(
+  "access_token",
+  tokenData.access_token
+);
+
+
+
+localStorage.setItem(
+  "user",
+  JSON.stringify(
+    tokenData.user || {}
+  )
+);
+
+
+
+// ======================
+// Return Success
+// ======================
 return {
+
 
  success:true,
 
+
  data:tokenData,
+
 
 };
 
 
+
+
+
 }
 catch(error){
+
 
 console.error(
 "LOGIN ERROR:",
@@ -69,16 +117,23 @@ error.response?.data || error.message
 );
 
 
+
 return {
 
+
  success:false,
+
 
  message:
  error.response?.data?.detail ||
  "Login gagal"
 
+
 };
 
+
+
 }
+
 
 }
